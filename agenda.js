fb32b33be005f64768c64a8946f8f9e4a6dcdc4a
rubenslemos <rubenslemos@gmail.com>
2021-08-23 09:40:03 -0300

    finalização do projeto

--- agenda.js
+++ agenda.js
@@ -5,6 +5,37 @@ var formNovoEvento = document.getElementById('formNovoEvento')
 var inputNomeEvento = document.getElementById('nomeEvento')
 var inputDataEvento = document.getElementById('dataEvento')
 var divMensagemErro = document.getElementById('mensagemErro')
+var tabelaEventos = document.getElementById('tabelaEventos')
+var listaEventos = []
+
+function atualizarTabelaEventos() {
+    if (listaEventos.length === 0) {
+        tabelaEventos.innerHTML = '<tr><td colspan="3">Nenhum evento</td></tr>'
+        return
+    }
+    tabelaEventos.innerHTML = ''
+    for (var i = 0; i < listaEventos.length; i++) {
+        var evento = listaEventos[i]
+        var linha = document.createElement('tr')
+        var celulaNome = document.createElement('td')
+        var celulaData = document.createElement('td')
+        var celulaAcoes = document.createElement('td')
+        var btnDelete = document.createElement('button')
+        btnDelete.setAttribute('data-evento', i)
+        btnDelete.classList.add('btn')
+        btnDelete.classList.add('btn-danger')
+        btnDelete.classList.add('btn-sm')
+        btnDelete.innerText = 'Deletar'
+        btnDelete.addEventListener('click', removerEvento)
+        celulaNome.innerText = evento.nome
+        celulaData.innerText = evento.data
+        celulaAcoes.appendChild(btnDelete)
+        linha.appendChild(celulaNome)
+        linha.appendChild(celulaData)
+        linha.appendChild(celulaAcoes)
+        tabelaEventos.appendChild(linha)
+    }
+}
 
 function cancelar() {
     inputDataEvento.value = ''
@@ -13,6 +44,7 @@ function cancelar() {
     inputDataEvento.classList.remove("is-invalid")
     divMensagemErro.classList.add('d-none')
     novoEvento.classList.add('d-none')
+    divMensagemErro.innerHTML = ''
 }
 
 function novoEventoValido(nomeEvento, dataEvento) {
@@ -58,15 +90,29 @@ function novoEventoValido(nomeEvento, dataEvento) {
     return validacaoOk
 }
 
-buttonNovoEvento.addEventListener('click', () => novoEvento.classList.remove('d-none'))
-buttonCancelar.addEventListener('click', cancelar)
-formNovoEvento.addEventListener('submit', (event) => {
+function salvarNovoEvento(event) {
     event.preventDefault()
     var nomeEvento = inputNomeEvento.value
     var dataEvento = inputDataEvento.value
     if (novoEventoValido(nomeEvento, dataEvento)) {
-        console.log('Evento Valido!')
+        listaEventos.push({
+            nome: nomeEvento,
+            data: dataEvento,
+        })
+        atualizarTabelaEventos()
+        cancelar()
     } else {
         console.log('Evento Invalido!')
     }
-})
+}
+
+function removerEvento(event) {
+    var posicao = event.target.getAttribute('data-evento')
+    listaEventos.splice(posicao, 1)
+    atualizarTabelaEventos()
+}
+
+buttonNovoEvento.addEventListener('click', () => novoEvento.classList.remove('d-none'))
+buttonCancelar.addEventListener('click', cancelar)
+formNovoEvento.addEventListener('submit', salvarNovoEvento)
+window.addEventListener('load', atualizarTabelaEventos)
